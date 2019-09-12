584537288758e603fe48913766df24f0efb6629f
anderson <anderdavid86@hotmail.com>
2019-09-12 12:36:32 -0500

    Se hacen retoques

--- js/script.js
+++ js/script.js
@@ -2,14 +2,11 @@ $(document).ready(function($) {
 	console.log("hello jquery");
 
 	$(".ancla").click(function(evento){
-      //Anulamos la funcionalidad por defecto del evento
-      evento.preventDefault();
-      //Creamos el string del enlace ancla
-      var codigo = "#" + $(this).data("ancla");
-
-      console.log(codigo);
-      //Funcionalidad de scroll lento para el enlace ancla en 3 segundos
-      $("html,body").animate({scrollTop: $(codigo).offset().top}, 2000);
+ 		evento.preventDefault();
+     
+		var codigo = "#" + $(this).data("ancla");
+		console.log(codigo);
+		$("html,body").animate({scrollTop: $(codigo).offset().top}, 1000);
     });
 });
 
