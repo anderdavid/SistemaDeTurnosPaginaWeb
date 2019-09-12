61cf6e1bc890d9641b4e784ee7fb0af21616f283
anderson <anderdavid86@hotmail.com>
2019-09-12 11:08:25 -0500

    Se implemta enlaces ancla con desplazamiento lento

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-/*$(document).ready(function($) {
-	alert("hello jquery");
+$(document).ready(function($) {
+	console.log("hello jquery");
+
+	$(".ancla").click(function(evento){
+      //Anulamos la funcionalidad por defecto del evento
+      evento.preventDefault();
+      //Creamos el string del enlace ancla
+      var codigo = "#" + $(this).data("ancla");
+
+      console.log(codigo);
+      //Funcionalidad de scroll lento para el enlace ancla en 3 segundos
+      $("html,body").animate({scrollTop: $(codigo).offset().top}, 2000);
+    });
 });
 
-function test(){
-	alert("test");
-}*/
+
+
